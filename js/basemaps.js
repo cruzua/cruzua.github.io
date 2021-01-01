45a672769b7d06394eceec178dff3e0829c54913
cruzua <48709635+cruzua@users.noreply.github.com>
2021-01-01 21:42:39 +0200

    Update basemaps.js

--- js/basemaps.js
+++ js/basemaps.js
@@ -1,6 +1,8 @@
+var vpAttr = '2021 <a href="http://www.velopoltava.org">ГО «ВелоПолтава»</a>';
+
 // набір карт www.thunderforest.com
 var ThunderforestUrl = 'https://tile.thunderforest.com/{id}/{z}/{x}/{y}.png?apikey=9bf46b3f55ae48059ee52fdfeb14e0b1',
-    ThunderforestAttr = '2020 <a href="http://www.velopoltava.org">ГО «ВелоПолтава»</a> | Maps © <a href="https://www.thunderforest.com">Thunderforest</a> | Data © <a href="https://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>';
+    ThunderforestAttr = vpAttr + ' | Maps © <a href="https://www.thunderforest.com">Thunderforest</a> | Data © <a href="https://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>';
 
 var ThunderforestOpenCycleMap = L.tileLayer(ThunderforestUrl, {id: 'Cycle', attribution: ThunderforestAttr}),
     ThunderforestTransport = L.tileLayer(ThunderforestUrl, {id: 'transport', attribution: ThunderforestAttr}),
@@ -18,6 +20,85 @@ var baseMapsTF = {
     "TF Neighbourhood": ThunderforestNeighbourhood
     };
 
+// набір карт www.visicom.ua
+var VisicomUrl = 'https://tms{s}.visicom.ua/2.0.0/{map}/{style}/{z}/{x}/{y}.png',
+    VisicomAttr = vpAttr + ' | Картографічні дані © 2021 «<a href="https://api.visicom.ua/">АТ Візіком</a>»';
+
+var VisicomBaseUA = L.tileLayer(VisicomUrl, {map: 'world,ua', style: 'base', attribution: VisicomAttr, subdomains: "123", maxZoom: 19, tms: true}),
+    VisicomUAwb = L.tileLayer(VisicomUrl, {map: 'world,ua', style: 'print', attribution: VisicomAttr, subdomains: "123", maxZoom: 19, tms: true}),
+    VisicomUAbg = L.tileLayer(VisicomUrl, {map: 'world,ua', style: 'base_background', attribution: VisicomAttr, subdomains: "123", maxZoom: 19, tms: true}), // без підписів
+    VisicomUAsing = L.tileLayer(VisicomUrl, {map: 'world,ua', style: 'base_sign', attribution: VisicomAttr, subdomains: "123", maxZoom: 19, tms: true}); // підписи
+
+
+// OpenStreetMap
+var osm = L.tileLayer('//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: vpAttr + ' | Map data © OpenStreetMap contributors'}),
+    osmGPS = L.tileLayer('https://{s}.gps-tile.openstreetmap.org/lines/{z}/{x}/{y}.png', {attribution: '<a href="https://www.openstreetmap.org/#&layers=G">' + 'OpenStreetMap public GPS traces</a>'}),
+    osmBw = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {attribution: vpAttr + ' | Map data © OpenStreetMap contributors'});
+
+// набір карт з горизонталями
+var uatopomap = L.tileLayer('https://dgm.gki.com.ua/map/rtile/carto_2012332222387848919/ua/{z}/{x}/{y}.png', {attribution: vpAttr + ' | Maps © <a href="https://gki.com.ua/">Науково-дослідний інститут геодезії і картографії</a> CC-BY-SA</a>)'}),
+    opentopomap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {attribution: vpAttr + ' | Kartendaten: © <a href="https://openstreetmap.org/copyright">OpenStreetMap</a>-Mitwirkende, SRTM | Kartendarstellung: © <a href="http://opentopomap.org/">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'}),
+    m4UMaps = L.tileLayer('https://tileserver.4umaps.com/{z}/{x}/{y}.png', {attribution: vpAttr + ' | © 4UMaps.com <a href="https://4umaps.com/" target="_parent">4UMaps</a> Data CC-By-SA by OpenStreetMap', maxZoom: 15});
+
+// набір карт різні
+var hikebike = L.tileLayer('http://toolserver.org/tiles/hikebike/{z}/{x}/{y}.png', {attribution: vpAttr + ' | Maps © <a href="">Hike Bike</a> CC-BY-SA</a>)'}),
+    watercolor = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', { attribution: vpAttr + ' | Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> | Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>', subdomains: 'abcd', minZoom: 1, maxZoom: 16, ext: 'png'}),
+    mapy_cz_tour = L.tileLayer('https://m{s}.mapserver.mapy.cz/turist-m/{z}-{x}-{y}', {attribution: '<a href="https://mapy.cz/turisticka">mapy.cz outdoor map</a>'}),
+    stravaHot = L.tileLayer('https://heatmap-external-a.strava.com/tiles-auth/all/hot/{z}/{x}/{y}.png?px=256', {attribution: '<a href="https://www.strava.com/heatmap">Strava Global Heatmap</a>'});
+
+var MapBoxSatellite = L.tileLayer('https://api.tiles.mapbox.com/v4/mapbox.satellite/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoidmVsb3BvbHRhdmEiLCJhIjoiY2tmeG1pcGh6MXM0ejJ6c3ZheG4yM3R2aCJ9.cO3Um57gnX9eGqA9kLT7Ug', {ver: '4', id: 'mapbox.satellite', attribution: vpAttr + ' | Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'});
+
+
+// набір карт MIX
+var baseMapsMix = {
+//   "<span style='color: gray'>Сіра</span>": MapBoxGrayscale,
+    "Мапа України": uatopomap,
+    "VisicomBaseUA": VisicomBaseUA,
+    "VisicomUAwb": VisicomUAwb,
+    "Супутник": MapBoxSatellite,
+//    "Outdoors": MapBoxOutdoors,
+    "TF Pioneer": ThunderforestPioneer,
+    "Акварель": watercolor,
+    "hikebike": hikebike,
+    "Топографічна": opentopomap,
+    "mapy.cz outdoor map": mapy_cz_tour
+    };
+
+
+var baseTree = {
+  label: "<b>БАЗОВА МАПА</b>",
+  noShow: false,
+  children: [
+    {
+      label: "<b>Звичайні</b>",
+      children: [
+        { label: "Мапа України", layer: uatopomap },
+        { label: "OpenStreeMap", layer: osm },
+        { label: "OSM сіра", layer: osmBw },
+        { label: "hike+bike", layer: hikebike },
+        { label: "Топографічна", layer: opentopomap }
+      ]
+    },
+    {
+      label: "<b>Спеціальні</b>",
+      children: [
+        { label: "TF Transport", layer: ThunderforestTransport },
+        { label: "TF Pioneer", layer: ThunderforestPioneer },
+        { label: "Акварель", layer: watercolor }
+      ]
+    },
+    {
+      label: "<b>Супутникові</b>",
+      children: [{ label: "Супутник", layer: MapBoxSatellite }]
+    }
+  ]
+};
+
+
+
+
+
+
 // набір карт www.mapbox.com
 var MapBoxAttr = '2020 <a href="http://www.velopoltava.org">ГО «ВелоПолтава»</a> | Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
                  '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a> | ' +
@@ -34,38 +115,4 @@ var baseMapsMapBox = {
     "<span style='color: gray'>Сіра</span>": MapBoxGrayscale,
     "Супутник": MapBoxSatellite,
     "Outdoors": MapBoxOutdoors
-    };
-
-// набір карт www.visicom.ua
-var VisicomUrl = 'https://tms{s}.visicom.ua/2.0.0/{map}/{style}/{z}/{x}/{y}.png',
-    VisicomAttr = '2020 <a href="http://www.velopoltava.org">ГО «ВелоПолтава»</a> | Картографічні дані © 2020 «<a href="https://api.visicom.ua/">АТ Візіком</a>»';
-
-var VisicomBaseUA = L.tileLayer(VisicomUrl, {map: 'world,ua', style: 'base', attribution: VisicomAttr, subdomains: "123", maxZoom: 19, tms: true}),
-    VisicomUAwb = L.tileLayer(VisicomUrl, {map: 'world,ua', style: 'print', attribution: VisicomAttr, subdomains: "123", maxZoom: 19, tms: true}),
-    VisicomUAbg = L.tileLayer(VisicomUrl, {map: 'world,ua', style: 'base_background', attribution: VisicomAttr, subdomains: "123", maxZoom: 19, tms: true}), // без підписів
-    VisicomUAsing = L.tileLayer(VisicomUrl, {map: 'world,ua', style: 'base_sign', attribution: VisicomAttr, subdomains: "123", maxZoom: 19, tms: true}); // підписи
-
-// OpenStreetMap
-var osm = L.tileLayer('//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: '© OpenStreetMap contributors'}),
-    osmBw = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {attribution: '© OpenStreetMap contributors'}),
-    osmGPS = L.tileLayer('https://{s}.gps-tile.openstreetmap.org/lines/{z}/{x}/{y}.png', {attribution: '<a href="https://www.openstreetmap.org/#&layers=G">' + 'OpenStreetMap public GPS traces</a>'}),
-    hikebike = L.tileLayer('http://toolserver.org/tiles/hikebike/{z}/{x}/{y}.png', {attribution: '2020 <a href="http://www.velopoltava.org">ГО «ВелоПолтава»</a> | Maps © <a href="">Hike Bike</a> CC-BY-SA</a>'});
-
-// набір карт різні
-var
-    uatopomap = L.tileLayer('https://dgm.gki.com.ua/map/rtile/carto_2012332222387848919/ua/{z}/{x}/{y}.png', {attribution: '2020 <a href="http://www.velopoltava.org">ГО «ВелоПолтава»</a> | Maps © <a href="https://gki.com.ua/">Науково-дослідний інститут геодезії і картографії</a> CC-BY-SA</a>'}),
-    opentopomap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {attribution: '2020 <a href="http://www.velopoltava.org">ГО «ВелоПолтава»</a> | Kartendaten: © <a href="https://openstreetmap.org/copyright">OpenStreetMap</a>-Mitwirkende, SRTM | Kartendarstellung: © <a href="http://opentopomap.org/">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'}),
-    m4UMaps = L.tileLayer('https://tileserver.4umaps.com/{z}/{x}/{y}.png', {attribution: '2020 <a href="http://www.velopoltava.org">ГО «ВелоПолтава»</a> | © 4UMaps.com <a href="https://4umaps.com/" target="_parent">4UMaps</a> Data CC-By-SA by OpenStreetMap', maxZoom: 15}),
-    watercolor = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', { attribution: '2020 <a href="http://www.velopoltava.org">ГО «ВелоПолтава»</a> | Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> | Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>', subdomains: 'abcd', minZoom: 1, maxZoom: 16, ext: 'png'});
-
-
-// набір карт MIX
-var baseMapsMix = {
-//   "<span style='color: gray'>Сіра</span>": MapBoxGrayscale,
-    "Мапа України": uatopomap,
-    "Супутник": MapBoxSatellite,
-//    "Outdoors": MapBoxOutdoors,
-    "TF Pioneer": ThunderforestPioneer,
-    "Акварель": watercolor,
-    "Топографічна": opentopomap
     };
