8e6b7d3e2a1d1e492e91a61c843b7f2667aa56f8
cruzua <48709635+cruzua@users.noreply.github.com>
2021-04-23 15:51:33 +0300

    Update basic.js

--- js/basic.js
+++ js/basic.js
@@ -1,58 +1,39 @@
-var urlwiki = 'https://uk.wikipedia.org/wiki/';
-var urlwikimedia = 'https://upload.wikimedia.org/wikipedia/commons/';
+// Shrink Navbar on Scroll
+window.onscroll = function () {  scrollFunction(); };
 
-
-var fortress = L.layerGroup();
-function style_gub(feature) { return { weight: 7, opacity: 1, color: '#ae7f2a', dashArray: '3', fillOpacity: 0.33, fillColor: '#ae7f2a' }; };
-L.geoJson(polygon_1709, {style: style_gub,}).addTo(fortress).bindPopup( function (layer) {return layer.feature.properties.Name;} );
-
-
-function styleIcons(feature) {
-				 return {icon: L.icon({iconUrl: 'https://cruzua.github.io/svg/h71_green.svg', iconSize: [32, 32]}),}
-			 	 };
-
-// function styleDivIcons(feature) {
-// 				 				 return {icon: L.divIcon({ }),}
-// 				 			 	 };
-
-
-function popPlaces(feature, layer) {
-		     var popupContent = '<b>' + feature.properties.poi_name + '</b><br>' + feature.properties.poi_opys ;
-		     layer.bindPopup(popupContent); };
-
-
-var places_1709 = L.geoJson(place_1709, {
-		    pointToLayer: function (feature, latlng) {
-		        return L.marker(latlng, styleIcons(feature));
-		    }, onEachFeature: popPlaces
-		});
-
-var events_1709 = L.geoJson(event_1709, {
-				pointToLayer: function (feature, latlng) {
-				    return L.marker(latlng, styleIcons(feature));
-				}, onEachFeature: popPlaces
-		});
-
-var title = new L.Control({position: 'topleft'});
-		title.onAdd = function(map) {
-					this._div = L.DomUtil.create("div", "info");
-					this.update(); return this._div; };
-		title.update = function() {
-					this._div.innerHTML = '<div class="d-inline-flex"> <nav class="navbar navbar-expand-lg navbar-light navbar-title bg-light2"> <button class="navbar-toggler toggler-title" type="button" data-toggle="collapse" data-target="#TogglerTitleMain" aria-controls="TogglerTitle" aria-expanded="false" aria-label="Toggle navigation"> <i class="fa fa-map-marker-alt"></i> </button> <div class="collapse navbar-collapse" id="TogglerTitleMain"> <h1>Шляхами союзної армії Шведського короля Карла ХІІ та Гетьмана України Івана Мазепи: від Гадяча до Переволочної<br><span class="badge badge-danger">Сайт працює в тестовому режимі</span></h1> </div> </nav></div>';
-					};
-
-var btnInfo = new L.Control({position: 'bottomleft'});
-		btnInfo.onAdd = function(map) {
-				 this._div = L.DomUtil.create("div", "btnInfo");
-				 this.update(); return this._div; };
-		btnInfo.update = function() {
-			   this._div.innerHTML = '<div class="leaflet-control-about leaflet-bar leaflet-control" aria-haspopup="true"><a type="button" class="btn p-0" data-toggle="modal" data-target="#modalInfo" title="Layers"><i class="fa fa-info"></i></a></div>';
-			 	 };
-
-var btnFb = new L.Control({position: 'topleft'});
-		btnFb.onAdd = function(map) {
-				 this._div = L.DomUtil.create("div", "btnInfo");
-				 this.update(); return this._div; };
-		btnFb.update = function() {
-				 this._div.innerHTML = '<div class="leaflet-control-about leaflet-bar leaflet-control" aria-haspopup="true"><a type="button" class="btn p-0" data-toggle="modal" data-target="#modalInfo" title="Layers"><i class="fab fa-facebook-f"></i></a></div>';
-				 };
+function scrollFunction() {
+var i, header_class;
+  if ( document.body.scrollTop > 150 || document.documentElement.scrollTop > 150 ) {
+    header_class = document.getElementsByClassName("header-bg-none");
+  for (i = 0; i < header_class.length; i++) {
+    header_class[i].className = header_class[i].className.replace( "header-bg-none", "header-bg" );
+  }
+    document.getElementById("navbar-hide").style.display = "none";
+    document.getElementById("navbar-hide").style.transition = "0.3s";
+    document.getElementById("Logo_MP").style.height = "3em";
+    document.getElementById("Logo_MP").style.transition = "0.3s";
+  } else {
+    header_class = document.getElementsByClassName("header-bg");
+  for (i = 0; i < header_class.length; i++) {
+    header_class[i].className = header_class[i].className.replace( "header-bg", "header-bg-none" );
+  }
+    document.getElementById("navbar-hide").style.display = "inherit";
+    document.getElementById("navbar-hide").style.transition = "0.3s";
+    document.getElementById("Logo_MP").style.height = "4em";
+    document.getElementById("Logo_MP").style.transition = "0.3s";
+  }
+}
+// W3Schools Accordion
+var acc = document.getElementsByClassName("W3Sch_Acc");
+var i;
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("W3Sch_Acc_active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "em";
+    }
+  });
+}
